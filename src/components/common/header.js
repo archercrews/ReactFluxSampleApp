ab0b45e3076f166d80e53a30bc71a040af8d7abe
Harrison <crewsha@gmail.com>
2017-07-18 16:38:51 -0400

    Added authors page component along with mock authors API

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -8,11 +8,12 @@ var Header = React.createClass({
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <a href="/" className="navbar-brand">
-                        <img src="images/Heroes_of_the_Storm_logo.png" />
+                        <img src="images/Heroes_of_the_Storm_logo.png" className="img-responsive" style={{maxHeight:'100%'}}/>
                     </a>
                     <ul className="nav navbar-nav">
                         <li><a href="/">Home</a></li>
                         <li><a href="/#about">About</a></li>
+                        <li><a href="/#authors">Authors</a></li>
                     </ul>
                 </div>
             </nav>
